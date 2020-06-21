c435927747033ca3f287227cfaf3d765f8c79839
saikiranmsk <msaikiran0728@gmail.com>
2020-06-21 18:11:01 +0530

    [Sai] refractor. added the login component

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,6 +1,36 @@
-import { Component, OnInit } from "@angular/core";
-import { environment } from "src/environments/environment";
-import { Book } from "src/app/models/whish";
+// import { Component, OnInit } from "@angular/core";
+// import { environment } from "src/environments/environment";
+// import { Book } from "src/app/models/whish";
+
+// @Component({
+//   selector: "app-login",
+//   templateUrl: "./login.component.html",
+//   styleUrls: ["./login.component.scss"],
+// })
+// export class LoginComponent implements OnInit {
+//   constructor() {}
+
+//   value: String;
+//   book: Book = new Book();
+//   ngOnInit() {
+//     console.log("***********" + this.book.bookId);
+//     this.value = environment.GET_BOOKS_COUNT;
+//   }
+// }
+import { Component, OnInit, Input, Inject } from "@angular/core";
+import {
+  Validators,
+  FormControl,
+  FormGroup,
+  FormBuilder,
+} from "@angular/forms";
+import { Login } from "src/app/models/login";
+import { MatSnackBar, MAT_DIALOG_DATA } from "@angular/material";
+import { MatDialogRef } from "@angular/material";
+import { ActivatedRoute, Router } from "@angular/router";
+import { NgxSpinnerService } from "ngx-spinner";
+import { HttpService } from "src/app/service/http.service";
+//import { DataService } from "src/app/service/data.service";
 
 @Component({
   selector: "app-login",
@@ -8,12 +38,99 @@ import { Book } from "src/app/models/whish";
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  /**
+   * login object about consist of
+   * 1)email
+   * 2)password feilds
+   */
+  login: Login = new Login("", "");
+  loginForm: FormGroup;
+  token: string;
 
-  value: String;
-  book: Book = new Book();
-  ngOnInit() {
-    console.log("***********" + this.book.bookId);
-    this.value = environment.GET_BOOKS_COUNT;
+  email = new FormControl(this.login.mailOrMobile, [
+    Validators.required,
+    Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+  ]);
+  password = new FormControl(this.login.password, [
+    Validators.required,
+    Validators.minLength(8),
+  ]);
+  showSpinner = false;
+  person = String;
+  constructor(
+    private snackBar: MatSnackBar,
+    public formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private spinner: NgxSpinnerService,
+    private httpservice: HttpService //public dialogRef: MatDialogRef<LoginComponent>
+  ) //private data: DataService // @Inject(MAT_DIALOG_DATA) public data: any
+  {}
+
+  ngOnInit() {}
+  /**
+   * Email validation
+   */
+  getErrorEmail() {
+    return this.email.hasError("required")
+      ? "You must enter a value"
+      : this.email.hasError("email")
+      ? "Not a valid email"
+      : "";
+  }
+  /**
+   * Password Validation
+   */
+  getErrorPassword() {
+    return this.password.hasError("required")
+      ? "You must enter a value"
+      : this.password.hasError("password")
+      ? "Min 6 Elements"
+      : "";
+  }
+  diableRadios = true;
+  favoriteSeason: string = "user";
+  seasons = ["user", "seller", "admin"];
+  isDisabled: boolean = true;
+  onlogin() {
+    this.spinner.show();
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.spinner.hide();
+      this.httpservice
+        .postRequest(this.favoriteSeason + "/login", this.login)
+        .subscribe(
+          (response: any) => {
+            if (response.status == 200) {
+              this.spinner.hide();
+              this.token = localStorage.getItem("token");
+              console.log(this.token);
+              this.snackBar.open("Login Successfull", "undo", {
+                duration: 2500,
+              });
+              if (this.favoriteSeason == "user") {
+                localStorage.setItem("token", response.obj);
+
+                this.router.navigate(["books"]);
+              }
+              if (this.favoriteSeason == "seller") {
+                localStorage.setItem("token", response.obj);
+
+                this.router.navigate(["seller/books"]);
+              } else if (this.favoriteSeason == "admin") {
+                localStorage.setItem("token", response.obj);
+
+                this.router.navigate(["admin/books"]);
+              }
+            } else {
+              this.spinner.hide();
+              this.snackBar.open("Login Failed", "undo", { duration: 2500 });
+            }
+          },
+          (error: any) => {
+            this.snackBar.open(error.error.message, "undo", { duration: 2500 });
+          }
+        );
+    }, 2000); //spinner
   }
 }
