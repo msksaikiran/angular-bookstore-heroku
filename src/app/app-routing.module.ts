7363eb66798d7adb7f52de6ff556ebd638a8d52b
saikiranmsk <msaikiran0728@gmail.com>
2020-06-22 14:45:01 +0530

    [Sai] refractor. added the login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
+import { BooksComponent } from "./components/books/books.component";
 
 const routes: Routes = [
   {
     path: "login",
     component: LoginComponent,
+  },
+  {
+    path: "books",
+    component: BooksComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,3 @@
-// import { BrowserModule } from "@angular/platform-browser";
-// import { NgModule } from "@angular/core";
-
-// import { AppRoutingModule } from "./app-routing.module";
-// import { AppComponent } from "./app.component";
-// import { LoginComponent } from "./component/login/login.component";
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-// @NgModule({
-//   declarations: [AppComponent, LoginComponent],
-//   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-//   providers: [],
-//   bootstrap: [AppComponent],
-// })
-// export class AppModule {}
-
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
@@ -22,17 +6,9 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 /* Angular Flex Layout */
 import { FlexLayoutModule } from "@angular/flex-layout";
-// import { NgxPaginationModule } from "ngx-pagination";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-
-// import { RegisterComponent } from "./components/register/register.component";
-// import { FooterComponent } from "./components/footer/footer.component";
-// import { BooksComponent } from "./components/books/books.component";
-
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
 import {
   MatInputModule,
   MatSnackBarModule,
@@ -49,32 +25,31 @@ import { MatListModule } from "@angular/material/list";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatTableModule } from "@angular/material/table";
 import { MatToolbarModule } from "@angular/material/toolbar";
-// import { DashboardComponent } from "./components/dashboard/dashboard.component";
-// import { ViewCartComponent } from "./components/view-cart/view-cart.component";
-// import { OrderSuccessComponent } from "./components/order-success/order-success.component";
-// import { GetbooksComponent } from "./components/getbooks/getbooks.component";
-// import { SortBooksPipe } from "./pipes/sort-books.pipe";
-
-// import { MatProgressBarModule } from "@angular/material/progress-bar";
-// import { ForgetpasswordComponent } from "./components/forgetpassword/forgetpassword.component";
-// import { RestpasswordComponent } from "./components/restpassword/restpassword.component";
-
 import { MatRadioModule } from "@angular/material/radio";
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { NgxSpinnerModule } from "ngx-spinner";
-
+import { MatSelectModule } from "@angular/material/select";
+import { MatDialogModule } from "@angular/material/dialog";
+import { LoginComponent } from "./components/login/login.component";
+import { BooksComponent } from "./components/books/books.component";
+// import { RegisterComponent } from "./components/register/register.component";
+// import { FooterComponent } from "./components/footer/footer.component";
+//import { RatingComponent } from "./components/rating/rating.component";
+// import { GiverateComponent } from "./components/giverate/giverate.component";
 // import { WhishlistComponent } from "./components/whishlist/whishlist.component";
 // import { OrderDetailsComponent } from "./components/order-details/order-details.component";
 // import { VerifyComponent } from "./components/verify/verify.component";
 // import { MatBadgeModule } from "@angular/material/badge";
 // import { SearchComponent } from "./components/search/search.component";
 // import { SearchPipe } from "./pipes/search.pipe";
-import { MatSelectModule } from "@angular/material/select";
-//import { RatingComponent } from "./components/rating/rating.component";
-// import { GiverateComponent } from "./components/giverate/giverate.component";
-import { MatDialogModule } from "@angular/material/dialog";
-import { LoginComponent } from "./components/login/login.component";
-import { BooksComponent } from "./components/books/books.component";
+// import { DashboardComponent } from "./components/dashboard/dashboard.component";
+// import { ViewCartComponent } from "./components/view-cart/view-cart.component";
+// import { OrderSuccessComponent } from "./components/order-success/order-success.component";
+// import { GetbooksComponent } from "./components/getbooks/getbooks.component";
+// import { SortBooksPipe } from "./pipes/sort-books.pipe";
+// import { MatProgressBarModule } from "@angular/material/progress-bar";
+// import { ForgetpasswordComponent } from "./components/forgetpassword/forgetpassword.component";
+// import { RestpasswordComponent } from "./components/restpassword/restpassword.component";
 //import { CookieService, CookieOptions } from "angular2-cookie/core";
 // import { AddbookComponent } from "./components/addbook/addbook.component";
 // import { SellerbooksComponent } from "./components/sellerbooks/sellerbooks.component";
